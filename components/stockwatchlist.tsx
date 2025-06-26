9aa476e424b5ec4240e3743a9a4838ee80d0ff36
oyanzhi <ongyanzhi15@gmail.com>
2025-06-26 18:35:51 +0800

    tried injection of table rows

--- components/stockwatchlist.tsx
+++ components/stockwatchlist.tsx
@@ -1,40 +1,75 @@
     import { useState } from 'react';
-    import { useRouter } from "next/navigation";
+
+    type Stock = {
+        ticker: string;
+        predictedclosing: number;
+    }
 
     export default function StockWatchList() {
-        const router = useRouter();
-
-        const [tickerlist, setTickerList] = useState<string[]>([]);
+        const [tickerlist, setTickerList] = useState<Stock[]>([]);
         const [input, setInput] = useState("");
 
         const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
             setInput(e.target.value);
         };
 
-        const addTicker = () => {
-            const trimticker = input.trim();
-            if (!tickerlist.includes(trimticker)) {
-                setTickerList((prev) => [...prev, trimticker]);
-                setInput("");
+        const addTicker = async () => {
+            const trimticker = input.trim().toUpperCase();
+            if (!tickerlist.some((s) => s.ticker === trimticker)) {
+                try {
+                    const response = await fetch("", {
+                        method: "POST",
+                        headers: { "Content-Type": "application/json"},
+                        body: JSON.stringify({ tickersymbol : [trimticker]})
+                    })
+
+                    if (!response.ok) {
+                        alert("Invalid Ticker");
+                        setInput("");
+                        return;
+                    }
+
+                    const result = await response.json();
+                    const price = result[trimticker];
+                        
+                    if (price === undefined) {
+                        alert("No Result");
+                        setInput("");
+                        return;
+                    }
+
+                    setTickerList(prev => [...prev, { ticker: trimticker, predictedclosing: price}]);
+                    setInput("");
+                    return;
+                } catch (error) {
+                    alert("Failed to Fetch Prediction");
+                    setInput("");
+                    return;
+                }
             }
+            alert("Ticker Already Added.");
+            setInput("");
+            return;
         }
 
         const removeTicker = () => {
-            const trimticker = input.trim();
-            if (tickerlist.includes(trimticker)) {
-                setTickerList((prev) => prev.filter(x => x !== trimticker));
+            const trimticker = input.trim().toUpperCase();
+            if (tickerlist.some((s) => s.ticker === trimticker)) {
+                setTickerList((prev) => prev.filter(x => x.ticker !== trimticker));
                 setInput("");
+                return;
             }
+            alert("Not In Watchlist")
+            setInput("");
         }
 
         const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
             e.preventDefault();
-
             if (input.length === 0) {
                 alert("Input is Empty")
                 return;
             }
-            addTicker();
+            return await addTicker();
         }
 
         return (
@@ -43,8 +78,8 @@
                 <div className="flex justify-center items-center h-16">
                     <form onSubmit={handleSubmit} className="flex items-center space-x-4">
                         <input placeholder="Enter Ticker" required value={input} name="ticker" className="w-64 px-4 py-2 rounded-lg bg-gray-800 text-white focus:outline-none" type="text" onChange={handleChange} />
-                        <button className="bg-buttonblue hover:bg-buttonhoverblue text-white px-4 py-2 rounded-xl font-semibold transition" type="submit" onClick={addTicker} >Add</button>
-                        <button className="bg-buttonblue hover:bg-buttonhoverblue text-white px-4 py-2 rounded-xl font-semibold transition" onClick={removeTicker}>Remove</button> 
+                        <button className="bg-buttonblue hover:bg-buttonhoverblue text-white px-4 py-2 rounded-xl font-semibold transition" type="submit">Add</button>
+                        <button className="bg-buttonblue hover:bg-buttonhoverblue text-white px-4 py-2 rounded-xl font-semibold transition" type="button" onClick={removeTicker}>Remove</button> 
                     </form>
                 </div>
 
@@ -54,14 +89,17 @@
                     <table className="table-auto border-separate border-spacing-y-2">
                         <thead>
                             <tr className="text-gray-400">
-                                <th scope="col" className="px-10 py-3 border-t border-b">Ticker</th>
-                                <th scope="col" className="px-10 py-3 border-t border-b">Predicted Next-Day Closing Price</th>
+                                <th scope="col" className="px-25 py-3 border-b">Ticker</th>
+                                <th scope="col" className="px-25 py-3 border-b">Predicted Next-Day Closing Price</th>
                             </tr>
                         </thead>
                         <tbody>
-                            <tr>
-                                <td>test</td>
-                            </tr>
+                            {tickerlist.map(({ticker, predictedclosing}) => (
+                                <tr className="text-gray-400">
+                                    <td className="px-25 py-3 border-t border-b">{ticker}</td>
+                                    <td className="px-25 py-5 border-t border-b">{predictedclosing}</td>
+                                </tr>
+                            ))}
                         </tbody>
                     </table>
                 </div>
