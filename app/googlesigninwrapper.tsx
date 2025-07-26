efef02d49091ebe9440160e7a6e419ea07ccc73f
oyanzhi <ongyanzhi15@gmail.com>
2025-07-27 00:46:25 +0800

    using vercel env

--- app/googlesigninwrapper.tsx
+++ app/googlesigninwrapper.tsx
@@ -2,7 +2,7 @@
 
 import { GoogleOAuthProvider } from '@react-oauth/google';
 
-const clientID = "617178650210-6qri3p95ros5u1qdvh6f851r7h49ppbh.apps.googleusercontent.com";
+const clientID = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID!;
 
 export default function GoogleSignInWrapper( {children}: {children: React.ReactNode} ) {
   return (
