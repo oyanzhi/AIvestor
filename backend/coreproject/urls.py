8ab8d6c3ed88d0f0037e3502e1c55188ddf197c6
oyanzhi <ongyanzhi15@gmail.com>
2025-07-27 05:03:16 +0800

    url editing

--- backend/coreproject/urls.py
+++ backend/coreproject/urls.py
@@ -30,5 +30,5 @@ urlpatterns = [
     path('profileUpdateRequest/', include('profileUpdateRequest.urls')),
     path('stocks/', include('stocks.urls')),
     path('portfolio/', include('portfolio.urls')),
-    path('notifications', include("notifications.urls"))
+    path('notifications/', include("notifications.urls"))
 ]
