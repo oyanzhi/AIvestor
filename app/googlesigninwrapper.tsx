f485146e989913df3f173da88849ecd6940f2853
oyanzhi <ongyanzhi15@gmail.com>
2025-07-27 00:40:36 +0800

    testing env

--- app/googlesigninwrapper.tsx
+++ app/googlesigninwrapper.tsx
@@ -2,7 +2,7 @@
 
 import { GoogleOAuthProvider } from '@react-oauth/google';
 
-const clientID = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID!;
+const clientID = "617178650210-6qri3p95ros5u1qdvh6f851r7h49ppbh.apps.googleusercontent.com";
 
 export default function GoogleSignInWrapper( {children}: {children: React.ReactNode} ) {
   return (
