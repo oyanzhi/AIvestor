02ff5c1f758d89bf2d2d43b2e9cac50c12e656e7
oyanzhi <ongyanzhi15@gmail.com>
2025-07-27 02:19:18 +0800

    backend for google login

--- backend/loginaccountapp/views.py
+++ backend/loginaccountapp/views.py
@@ -6,6 +6,22 @@ from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework.permissions import AllowAny
 from django.http import HttpResponse
 
+from google.oauth2 import id_token
+from google.auth.transport import requests
+from django.conf import settings
+
+from registeraccountapp.models import AccountDatabase
+
+import string
+import secrets
+from django.contrib.auth.hashers import make_password
+
+def make_random_password():
+    alphabet = string.ascii_letters + string.digits
+    password = ''.join(secrets.choice(alphabet) for i in range(8))
+
+    return make_password(password)
+
 # Create your views here.
 class LoginAccountAppView(APIView):
     permission_classes = [AllowAny]
@@ -23,8 +39,6 @@ class LoginAccountAppView(APIView):
             
             response = Response({"message": "Login Successful", "access": access_token}, status=status.HTTP_200_OK)
 
-            print("token:", access_token)
-
             response.set_cookie(
                 key="refresh_token",
                 value=refresh_token,
@@ -37,6 +51,76 @@ class LoginAccountAppView(APIView):
             return response
         
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+class GoogleSignInView(APIView):
+    permission_classes = [AllowAny]
+    
+    def post(self, request):
+        token = request.data.get("id_token")
+
+        if not token:
+            return Response({"Error": "ID token missing."}, status=status.HTTP_400_BAD_REQUEST)
+        
+        try:
+            info = id_token.verify_oauth2_token(token, requests.Request(), audience=settings.GOOGLE_CLIENT_ID)
+
+        except Exception as e:
+            return Response({"Error": str(e)}, status=status.HTTP_401_UNAUTHORIZED)
+        
+        email = info.get("email")
+        sub = info.get("sub")
+
+        if not email or not sub:
+            return Response({"Error": "Missing Data"}, status=status.HTTP_400_BAD_REQUEST)
+        
+        try:
+            user = AccountDatabase.objects.get(oauth_provider="google", oauth_provider_id=sub)
+
+        except AccountDatabase.DoesNotExist:
+            try:
+                user = AccountDatabase.objects.get(email=email)
+                user.oauth_provider = "google"
+                user.oauth_provider_id = sub
+                user.is_oauth_user = True
+                user.save()
+
+            except AccountDatabase.DoesNotExist:
+                base_username = email.split("@")[0]
+                username = base_username
+                counter = 1
+
+                while AccountDatabase.objects.filter(username=username).exists():
+                    username = f"{base_username}{counter}"
+                    counter += 1
+
+                user = AccountDatabase.objects.create(
+                    username = username,
+                    email = email,
+                    password = make_random_password(),
+                    oauth_provider = "google",
+                    oauth_provider_id = sub,
+                    is_oauth_user = True,
+                )
+        
+        token = RefreshToken.for_user(user)
+        access_token = str(token.access_token)
+
+        refresh_token = str(token)
+        
+        response = Response({"message": "Login Successful", "access": access_token}, status=status.HTTP_200_OK)
+
+        response.set_cookie(
+            key="refresh_token",
+            value=refresh_token,
+            httponly=True,
+            secure=True,
+            samesite="Lax",
+            max_age= 7 * 24 * 60 * 60 #same timeframe as refresh token
+        )
+
+        return response
+
+
     
 class TokenRefreshView(APIView): #currently setup but not being used
     permission_classes = [AllowAny]
