045642d9cbd6913bdcde66922b55b5b0260bc1b3
oyanzhi <ongyanzhi15@gmail.com>
2025-07-27 00:32:46 +0800

    testing google button rendering

--- app/login/LoginPage.tsx
+++ app/login/LoginPage.tsx
@@ -5,10 +5,22 @@ import LoginForm from "../../components/forms/loginform";
 import Link from 'next/link';
 import { useSearchParams } from 'next/navigation';
 import Footer from "../../components/sidebar/footer";
+import { GoogleLogin, CredentialResponse } from "@react-oauth/google";
 
 function LoginPage() {
     const searchParams = useSearchParams();
     const successfulRegistration = searchParams.get('successfulRegistration') === 'true';
+
+    const handleSuccess = (credentialResponse: CredentialResponse) => {
+        console.log(credentialResponse.credential);
+        //testing
+    }
+
+    const handleFailure = () => {
+        alert("Google Login Failed.");
+        //testing
+    }
+
     return (
         <div className="flex flex-col min-h-screen bg-deepblue">
             <LoginNavBar />
@@ -22,6 +34,7 @@ function LoginPage() {
                         Don’t have an account? <Link href="/register" className="text-cyan-400 hover:underline">Register here</Link>
                     </p>
                 </div>
+                <GoogleLogin onSuccess={handleSuccess} onError={handleFailure}/>
             </main>
             
             <Footer />
