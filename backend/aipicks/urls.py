8369f9187c1b759b4167496f19517a44e0f22ed3
oyanzhi <ongyanzhi15@gmail.com>
2025-07-27 22:31:38 +0800

    frontend integration

--- backend/aipicks/urls.py
+++ backend/aipicks/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import AddWatchlist, FetchWatchlist, FetchAIRecommendations
+from .views import AddWatchlist, RemoveWatchlist, FetchWatchlist, FetchAIRecommendations
 
 urlpatterns = [
     path("addwatchlist/", AddWatchlist.as_view(), name="addwatchlist"),
+    path("removewatchlist/", RemoveWatchlist.as_view(), name="removewatchlist"),
     path("fetchwatchlist/", FetchWatchlist.as_view(), name="fetchwatchlist"),
     path("fetchairecommendations/", FetchAIRecommendations.as_view(), name="fetchairecommendations"),
 ]
